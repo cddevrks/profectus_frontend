7949a6f9d0d61eabf9667c0c0d0bb7919f936a75
RangaKomalSai <ranga.komal.sai@gmail.com>
2025-11-22 07:55:15 +0530

    env fix

--- src/utils/apiConfig.jsx
+++ src/utils/apiConfig.jsx
@@ -1,4 +1,3 @@
 // export const API_URL = "http://localhost:5000";
 
-export const API_URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:4001";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4001";
